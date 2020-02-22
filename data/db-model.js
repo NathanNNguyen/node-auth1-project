fc16bcd69551505720af546928b993957fc65ae5
Nhan <natenguyen51294@gmail.com>
2020-02-21 20:16:11 -0800

    added middleware to protect data

--- data/db-model.js
+++ data/db-model.js
@@ -2,7 +2,7 @@ const db = require('./db-config.js');
 
 module.exports = {
   findUsers,
-  addUser,
+  add,
   findBy
 }
 
@@ -10,7 +10,7 @@ function findUsers() {
   return db('users');
 };
 
-function addUser(userData) {
+function add(userData) {
   return db('users')
     .insert(userData)
 
--- server.js
+++ server.js
@@ -10,21 +10,11 @@ server.get('/', (req, res) => {
   res.send(`It's working`);
 });
 
-server.get('/api/users', async (req, res) => {
-  try {
-    const users = await db.findUsers();
-    res.status(200).json(users);
-  }
-  catch (err) {
-    res.status(500).json({ message: 'Invalid username or password' })
-  }
-});
-
 server.post('/api/register', async (req, res) => {
   const user = req.body;
-  user.password = bcrypt.hashSync(user.password, 10);
+  user.password = bcrypt.hashSync(user.password, 10); // hash password
   try {
-    const inserted = await db.addUser(user);
+    const inserted = await db.add(user);
     res.status(201).json(inserted);
   }
   catch (err) {
@@ -36,8 +26,7 @@ server.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await db.findBy({ username });
-
-    if (user && bcrypt.compareSync(password, user.password)) {
+    if (user && bcrypt.compareSync(password, user.password)) { // real password first then db password
       res.status(200).json({ message: `Welcome ${user.username}` })
     } else {
       res.status(401).json({ message: `Incorrect username or password` })
@@ -46,6 +35,35 @@ server.post('/api/login', async (req, res) => {
   catch (err) {
     res.status(500).json({ message: 'Invalid username or password', err })
   }
-})
+});
+
+server.get('/api/users', restricted, async (req, res) => {
+  try {
+    const users = await db.findUsers();
+    res.status(200).json(users);
+  }
+  catch (err) {
+    res.status(500).json({ message: 'Invalid username or password' })
+  }
+});
+
+async function restricted(req, res, next) {
+  const { username, password } = req.headers;
+  try {
+    if (username && password) {
+      const user = await db.findBy({ username })
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' })
+      }
+    } else {
+      res.status(400).json({ message: 'Must have valid username / password' })
+    }
+  }
+  catch (err) {
+    res.status(500).json({ message: 'Unexpected error' })
+  }
+}
 
 module.exports = server;
